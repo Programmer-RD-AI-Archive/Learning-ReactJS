569b381b154904daa8cbc1807d902c68cdc79495
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-06-12 22:18:08 +0530

    Remove Unnecessary Files

--- 4/usestate/src/App.js
+++ 4/usestate/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(() => {
+    return 0;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          // setCount(count - 1);
+          setCount((prevCount) => prevCount - 1);
+        }}
+      >
+        -
+      </button>
+      <span>{{ count }}</span>
+      <button
+        onClick={() => {
+          setCount((prevCount) => prevCount + 1);
+        }}
+      >
+        +
+      </button>
+    </>
   );
 }
 
